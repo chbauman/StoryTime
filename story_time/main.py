00d7383c2334f15b9c3892a85f9eb780a0fbdf49
Christian Baumann <chbauman@student.ethz.ch>
2020-11-02 20:41:11 +0100

    Added todo

--- story_time/main.py
+++ story_time/main.py
@@ -10,10 +10,12 @@ TODO:
     - Make header color changeable: https://stackoverflow.com/questions/51000320/wxpython-change-the-headers-color
     - Host documentation somewhere. (Write it first! (Including automatic screenshot generation!??))
     - Indicate diary entries in calendar. (Probably need a custom calendar... arghhh)
+    - Video Capture: Check for multiple input and add Dialog
     - Smoother UI changes (e.g. image loading)
     - Preview: Resolve issue with multiple entries on same Date and time.
     - Center toolbar?
     - Handle image deletion while app is running
+    - Add blogpost to personal website
     - Add screenshots to readme.md
     - Fix Message dialogs (Including base class) What is the wx.Panel needed for?????.
     - Calendar: Add 'now' option that sets the time to now (or set default selected time to now)
